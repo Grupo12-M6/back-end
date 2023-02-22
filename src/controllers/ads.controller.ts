c7d1df5400ae1163ebb292e0abdf056e24d70d73
Lucas Mendes <lucasilveiramendes@gmail.com>
2023-02-22 08:58:57 -0300

    feat: att organization function ads

--- src/controllers/ads.controller.ts
+++ src/controllers/ads.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 import { instanceToPlain } from "class-transformer";
 
-import { createAdsService } from "../services/ads/createAds.service";
 import { IAdsRequest } from "../interfaces/ads";
+
+import { createAdsService } from "../services/ads/createAds.service";
+
+import deleteAds from "../services/ads/deleteAds.service";
 
 const createAdsController = async(req: Request, res: Response) => {
     const data: IAdsRequest = req.body;
@@ -13,6 +16,14 @@ const createAdsController = async(req: Request, res: Response) => {
     return res.status(201).json(instanceToPlain(response))
 }
 
+const deleteAdsController = async (req: Request, res: Response) => {
+    const idAd = req.params.idAd;
+    await deleteAds(idAd);
+  
+    return res.status(204).json();
+};
+
 export {
     createAdsController,
+    deleteAdsController
 }

e094018630960cdcbb48b66c43ffc3b04bb87185
EliseuBrito <eliseubrito@protonmail.com>
2023-02-27 10:58:11 -0300

    feat: created service and controller for listOneAd

--- src/routes/ads.routes.ts
+++ src/routes/ads.routes.ts
@@ -5,7 +5,8 @@ import {
   deleteAdsController,
   isActiveAdsController,
   updateAdsController,
-  listAdsController
+  listAdsController,
+  listOneAdController,
 } from "../controllers/ads.controller";
 import { authTokenMiddleware } from "../middlewares/authToken.middleware";
 
@@ -14,6 +15,7 @@ const routes = Router();
 const adsRoutes = () => {
   routes.post("/", authTokenMiddleware, createAdsController);
   routes.patch("/:id", authTokenMiddleware, updateAdsController);
+  routes.get("/:id", authTokenMiddleware, listOneAdController);
   routes.delete("/:id", authTokenMiddleware, deleteAdsController);
   routes.delete("isActive/:id", authTokenMiddleware, isActiveAdsController);
   routes.get("/", listAdsController);

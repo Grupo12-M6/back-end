b4f0db373b60d950defa73a53e2274cac0b3dd17
Agnes-lica <agnes.lica@gmail.com>
2023-02-28 09:55:55 -0400

    feat: user Delete

---
+++ src/services/users/deleteUser.service.ts
@@ -0,0 +1,20 @@
+import dataSource from "../../data-source";
+import { AppError } from "../../errors/appError";
+
+import { User } from "../../entities/user.entity";
+
+const deleteUserService = async (id: string): Promise<boolean> => {
+  const userRepository = dataSource.getRepository(User);
+
+  const user = await userRepository.findOneBy({ id });
+
+  if (!user) {
+    throw new AppError("This Ad does not exist!", 404);
+  }
+
+  await userRepository.delete(id);
+
+  return true;
+};
+
+export default deleteUserService;

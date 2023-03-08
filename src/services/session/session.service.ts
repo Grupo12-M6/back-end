973435df68652310ed4b3c1451f7f54f7a1d111d
EliseuBrito <eliseubrito@protonmail.com>
2023-03-08 11:27:24 -0300

    feat: add archives for execute

--- src/services/session/session.service.ts
+++ src/services/session/session.service.ts
@@ -2,37 +2,45 @@ import dataSource from "../../data-source";
 import { AppError } from "../../errors/appError";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import "dotenv/config"
+import "dotenv/config";
 
 import { User } from "../../entities/user.entity";
 import { IUserLogin } from "../../interfaces/users";
 
-export const sessionService = async({
-    email, password
+export const sessionService = async ({
+  email,
+  password,
 }: IUserLogin): Promise<string> => {
+  const userRepository = dataSource.getRepository(User);
+  const user = await userRepository.findOneBy({ email });
 
-    const userRepository = dataSource.getRepository(User);
-    const user = await userRepository.findOneBy({ email });
+  if (!user) {
+    throw new AppError("Account not found");
+  }
 
-    if(!user) {
-        throw new AppError( "Account not found" );
+  if (user.isDelete) {
+    throw new AppError("User has been deleted!", 404);
+  }
+
+  if (!user.password) {
+    throw new AppError("Password not found", 401);
+  }
+  if (!bcrypt.compareSync(password.toString(), user.password)) {
+    throw new AppError("Wrong email/password", 403);
+  }
+
+  if (user.active == false) {
+    throw new AppError("User not activate!", 403);
+  }
+
+  const token = jwt.sign(
+    { email: user.email },
+    process.env.SECRET_KEY as string,
+    {
+      subject: user.id,
+      expiresIn: "10h",
     }
+  );
 
-    if (user.isDelete) {
-        throw new AppError("User has been deleted!", 404);
-    }
-
-    if(!user.password) {
-        throw new AppError( "Password not found", 401 );
-    }
-    if (!bcrypt.compareSync(password.toString(), user.password)){
-        throw new AppError("Wrong email/password", 403)
-    };
-
-    const token = jwt.sign({email: user.email}, process.env.SECRET_KEY as string, {
-        subject: user.id,
-        expiresIn: "10h"
-    })
-
-    return token
-}
+  return token;
+};
